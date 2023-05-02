938b91dffcf26ca34aa70b3d3fcc4f753f7288d1
Onedrift <fantasia.6549658256@gmail.com>
2023-05-02 14:54:43 +0200

    canbiato inversione

--- inversione.c
+++ inversione.c
@@ -1,38 +1,49 @@
+//elimina una posizione dell'array
+
 #include <stdio.h>
-#define N[10];
+#define MAX_SIZE 100
 
-void riempi (int a[], int t)																
+int main()
 {
-	int i=0;
-	for(i;i<t;i++)
-	{
-		scanf("%d",a+i);
-	}
-}
+    int arr[MAX_SIZE];
+    int i, grandezza, posizione;
 
-	void scambia (int a[], int t)
-	{
-		int i=0,x;
-		fro (a;a<N+N/2;a+i)
-		{
-			x=a+i;
-			*(a+i)=*(a+N-i-1);
-			*(a+N-i-1)=x;
-			i++;
-		}
-	}
-		
-	
+    
+    printf("inserire la grandezza di un array \n ");
+    scanf("%d", &grandezza);
+    printf("inserire gli elementi in un array \n ");
+    for(i=0; i<grandezza; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
 
-int main (void)
-{
-	int a[N];
-	riempi (a,'N');
-	scambia (a,'N');
-	printf("visualizza scambia %d", scambia);
-	
-	
-	
+    printf("seleziona la posizione dell'array da eliminare \n ");
+    scanf("%d", &posizione);
 
-	return 0;
+
+    
+    if(posizione < 0 || posizione > grandezza)
+    {
+        printf("posizione non valida inserire una posizione valida tra 1 ed %d", grandezza);
+    }
+    else
+    {
+        
+        for(i=posizione-1; i<grandezza-1; i++)
+        {
+            arr[i] = arr[i + 1];
+        }
+
+        
+        grandezza--;
+
+        
+        printf("\n gli elementi dell'array sono \n ");
+        for(i=0; i<grandezza; i++)
+        {
+            printf("%d\t", arr[i]);
+        }
+    }
+
+    return 0;
 }
